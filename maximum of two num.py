834237afd800f2d51130da4d877862f3aaa2618d
Amey Prakash <aditiaditi737@gmail.com>
2025-11-18 22:16:55 +0530

    max of two numbers

---
+++ maximum of two num.py
@@ -0,0 +1,23 @@
+a = 10
+b = 20
+print(max(a, b))
+
+
+c = 30 
+d = 40
+print(c if c > d else d)
+
+
+e = 50
+f = 60
+if(e > f):
+    print(e)
+else:
+    print(f)
+
+
+g = 70
+h = 80
+num = [g, h]
+num.sort()
+print(num[-1])
