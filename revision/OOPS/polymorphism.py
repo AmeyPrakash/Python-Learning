724175bd25701db09f8da3b0f775b994761cf041
Amey Prakash <aditiaditi737@gmail.com>
2026-03-09 20:03:49 +0530

    intro to Polymorphism

---
+++ revision/OOPS/polymorphism.py
@@ -0,0 +1,8 @@
+        #POLYMORPHISM
+#when the same operator or function behaves differently for different types of data, it is called polymorphism.
+
+#operator overloading
+# print(2+3) #5
+
+# print("Hello"+"World") #HelloWorld  
+# print([1,2]+[3,4]) #[1, 2, 3, 4] merge
