2b89d75d94ef4cf3e3de23ae7a2c97e5fb7dc7ab
Amey Prakash <aditiaditi737@gmail.com>
2026-03-11 14:52:27 +0530

    learned about polymorphism (dunder functions)

--- revision/OOPS/polymorphism.py
+++ revision/OOPS/polymorphism.py
@@ -6,3 +6,34 @@
 
 # print("Hello"+"World") #HelloWorld  
 # print([1,2]+[3,4]) #[1, 2, 3, 4] merge
+
+
+class complex:
+        def __init__(self,real,imaginary):
+                self.real=real
+                self.imaginary=imaginary
+        
+        def shownumber(self):
+                print(f"{self.real}+{self.imaginary}j")
+
+        def __add__(self,num2):
+                real=self.real+num2.real
+                imaginary=self.imaginary+num2.imaginary
+                return complex(real,imaginary)
+        def __sub__(self,num2):
+                real = self.real - num2.real
+                imaginary = self.imaginary - num2.imaginary
+                return complex(real, imaginary)
+              
+    
+num1 = complex(2,3)
+num1.shownumber() #2+3j   
+
+num2 = complex(5, 6)
+num2.shownumber() #5+6j
+
+num3 = num1 + num2
+num3.shownumber() #7+9j
+
+num4 = num1 - num2
+num4.shownumber() #-3-3j
