a8e7b467e6d10e8f33759022574061b0a2f64d3b
Amey Prakash <aditiaditi737@gmail.com>
2026-03-19 19:51:04 +0530

    intro to numpy

---
+++ numpy/intro.py
@@ -0,0 +1,14 @@
+#Numpy os the fundamental package for scientific computing in python
+#Numpy is a python library that provides a multidimensional array object various derived objects
+
+#arr=[123456]
+#list=[1,2,3,4,5]
+import numpy as np
+
+x=np.array([1,2,3,4,5])
+print(x)
+print(type(x))
+
+y=[1,2,3,4,5]
+print(y)
+print(type(y))
