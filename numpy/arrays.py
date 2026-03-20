242f195e9b18d652fcc24f0cbea810eb8859f69f
Amey Prakash <aditiaditi737@gmail.com>
2026-03-20 19:57:18 +0530

    creating Arrays

---
+++ numpy/arrays.py
@@ -0,0 +1,26 @@
+#ARRAY
+import numpy as np
+
+# x=[1,2,3,4,]
+# y=np.array(x)  #or  y=np.array([1,2,3,4,])
+# print(type(y))
+# print(y)
+
+
+# x=[]
+
+# for i in range(1,5):
+#     int_n=int(input("Enter the data"))
+#     x.append(int_n)
+
+# print(np.array(x))
+
+
+# y=np.array([[[1,2,3,4,],[1,2,3,4,],[1,2,3,4,]]])
+# print(y)
+# print(y.ndim)
+
+# arr = np.array([1, 5, 3, 4], ndmin=10)
+# print(arr)
+# print(arr.ndim)
+
