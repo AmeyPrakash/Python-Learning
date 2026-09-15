a620cd53567d11001ef85480edfd2ea210075fdd
Amey Prakash <aditiaditi737@gmail.com>
2026-09-15 21:48:25 +0530

    Search insert Position

---
+++ Arrays/Search_insert_position.py
@@ -0,0 +1,20 @@
+from typing import List
+class Solution:
+    def searchInsert(self, nums: list[int], target: int) -> int:
+        n = len(nums)
+        low = 0
+        high = n - 1
+
+        while low <= high:
+            mid = low + (high - low) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            elif nums[mid] < target:
+                low = mid + 1
+
+            else:
+                high = mid - 1
+
+        return low
